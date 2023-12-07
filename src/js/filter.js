02bd07371b9002292a4f9590019e5222a0c13c25
DmGle <dimon.gleda1@gmail.com>
2023-12-07 18:29:18 +0200

    search by filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { createProductItemMarkup } from './markup.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const filterForm = document.getElementById('filterForm');
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const productsList = document.getElementById('productsList');
 
     const filters = { keyword: null, category: null, page: 1, limit: 6 };
-    let categories = []; 
+    let categories = [];
 
     filterForm.addEventListener('submit', async (event) => {
         event.preventDefault();
@@ -17,9 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
         await fetchProducts();
     });
 
+    keywordInput.addEventListener('input', () => {
+        filters.keyword = keywordInput.value;
+        filters.page = 1;
+        fetchProducts();
+    });
+
+    categorySelect.addEventListener('change', () => {
+        filters.category = categorySelect.value === 'Show all' ? null : categorySelect.value;
+        filters.page = 1;
+        fetchProducts();
+    });
+
     const fetchProducts = async () => {
         try {
-            const url = `https://food-boutique.b.goit.study/api/products?page=${filters.page}&limit=${filters.limit}`;
+            let url = `https://food-boutique.b.goit.study/api/products?page=${filters.page}&limit=${filters.limit}`;
+    
+            if (filters.keyword) {
+                url += `&keyword=${filters.keyword}`;
+            }
+    
+            if (filters.category && filters.category !== 'Show all') {
+                url += `&category=${filters.category}`;
+            }
+    
             const response = await axios.get(url);
             const data = response.data;
             displayProducts(data.results);
@@ -29,14 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const displayProducts = (products) => {
-        productsList.innerHTML = '<h2>Products</h2><ul>' +
-            products.map(product =>
-                `<li>
-                    <img src="${product.img}" alt="${product.name}" />
-                    <p>${product.name}</p>
-                    <p>Price: $${product.price}</p>
-                </li>`
-            ).join('') +
+        productsList.innerHTML = '<ul class="product__list">' +
+            products.map(product => createProductItemMarkup(product)).join('') +
             '</ul>';
     };
 
