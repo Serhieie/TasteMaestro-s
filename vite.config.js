0620114c63af59db66d3d324beeee1962efc6406
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-12-08 18:54:36 +0200

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
@@ -11,7 +10,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -26,5 +24,8 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    optimizeDeps: {
+      include: ['src/img/sprite.svg'],
+    },
   };
 });
