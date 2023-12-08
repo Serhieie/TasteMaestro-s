0a24ed980b6e34b2c0669abe0c8de62ac7b2ad60
Vitalii Kravchenko <opabbk@gmail.com>
2023-12-08 19:34:03 +0200

    markup-js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import './js/commons';
 import './js/apiService';
 import './js/helpers';
 import './js/localStorage';
+<<<<<<< Updated upstream
 import './js/filter';
 import './js/footer';
 import './js/popular';
 import './js/pagination';
+=======
+import './js/footer';
+import './js/discount';
+>>>>>>> Stashed changes
