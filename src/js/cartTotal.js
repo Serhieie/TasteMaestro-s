d102cb77075116d2ae1597e3315a3a7d8a74b54f
kiljestyn <137730997+kiljestyn@users.noreply.github.com>
2023-12-08 21:50:55 +0200

    cart-total

--- src/js/cartTotal.js
+++ src/js/cartTotal.js
@@ -1,11 +1,11 @@
 import { getOrders } from "./apiService";
 
 
-// function calculateTotalPrice() {
-//   const totalPriceDiv = document.getElementById('total-price');
-//   const total = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-//   totalPriceDiv.textContent = `Total Price: ${total}`;
-// }
+function calculateTotalPrice() {
+  const totalPriceDiv = document.getElementById('total-price');
+  const total = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+  totalPriceDiv.textContent = `Total Price: ${total}`;
+}
 
 
 const checkoutForm = document.getElementById('checkoutForm');
