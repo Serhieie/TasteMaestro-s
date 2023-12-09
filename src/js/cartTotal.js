3138553f0344ac1467a5737d40d29a6072d054cb
kiljestyn <137730997+kiljestyn@users.noreply.github.com>
2023-12-10 00:15:04 +0200

    cartTotal

--- src/js/cartTotal.js
+++ src/js/cartTotal.js
@@ -1,6 +1,7 @@
 import { getOrders } from "./apiService";
 import AWN from 'awesome-notifications';
 import 'awesome-notifications/dist/style.css';
+import { showModalFirstCase, showModalSecondCase } from './modals';
 
 // function calculateTotalPrice() {
 //   const totalPriceDiv = document.getElementById('total-price');
