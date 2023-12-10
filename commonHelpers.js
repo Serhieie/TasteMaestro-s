aecd71f7dd431166862a01ab12d97aabb9901228
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-12-10 11:54:39 +0000

    Deploying to gh-pages from @ Serhieie/TasteMaestro-s@e197ad630714def883e7607b8babb35d3ae38762 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{s as f}from"./assets/sprite-223b7e65.js";import"./assets/vendor-f728e3bf.js";const o="cartItems";document.querySelector(".js-cart");const u=document.querySelector(".header__js_span"),s=e=>{try{const t=localStorage.getItem(e);return t===null?void 0:JSON.parse(t)}catch(t){console.error("Get state error: ",t.message)}};function v(){const e=document.querySelector(".header__js_span");s(o)?(u.textContent=`CART (${s(o).length})`,e.textContent=`CART (${s(o).length})`):(u.textContent=`CART (${0})`,e.textContent=`CART (${0})`)}window.addEventListener("load",v);const c=document.querySelector(".cart-order-list"),S=document.querySelector(".cart-filled-list"),q=document.querySelector(".cart-empty"),$=document.querySelector(".cart-clear-btn"),m=document.querySelector(".cart-count"),x=document.querySelector(".cart_total_order"),p=document.querySelector(".header__js_span");let r=JSON.parse(localStorage.getItem("cartItems"))||[];function L(e){const t=e.target;t.classList.contains("cart-order-decr")?_(t):t.classList.contains("cart-order-incr")?I(t):t.classList.contains("cart-remove-btn")&&y(t)}function b(e){return e.map(t=>`
+import{s}from"./assets/sprite-cc122293.js";import"./assets/vendor-f728e3bf.js";const d="cartItems";document.querySelector(".js-cart");const u=document.querySelector(".header__js_span"),m=e=>{try{const t=localStorage.getItem(e);return t===null?void 0:JSON.parse(t)}catch(t){console.error("Get state error: ",t.message)}};function C(){m(d)?u.textContent=`CART (${m(d).length})`:u.textContent=`CART (${0})`}window.addEventListener("load",C);const a=document.querySelector(".cart-order-list"),S=document.querySelector(".cart-filled-list"),$=document.querySelector(".cart-empty"),q=document.querySelector(".cart-clear-btn"),p=document.querySelector(".cart-count"),x=document.querySelector(".cart_total_order"),g=document.querySelector(".header__js_span");let r=JSON.parse(localStorage.getItem("cartItems"))||[];function L(e){const t=e.target;t.closest(".cart-order-decr")?I(t):t.closest(".cart-order-incr")?E(t):t.classList.contains("cart-remove-btn")&&v(t)}function b(e){return e.map(t=>`
   <li class="cart-order-item">
     <div class="cart-item-content">
       <div class="cart-order-img-cont">
@@ -16,7 +16,7 @@ import{s as f}from"./assets/sprite-223b7e65.js";import"./assets/vendor-f728e3bf.
               <svg class="cart-remove-icon" width="18" height="18">
                 <use
                   class="cart-remove-svg"
-                  xlink:href="${f}#close-cross"
+                  xlink:href="${s}#close-cross"
                 ></use>
               </svg>
             </span>
@@ -30,13 +30,23 @@ import{s as f}from"./assets/sprite-223b7e65.js";import"./assets/vendor-f728e3bf.
         <div class="cart-order-total-price">
           <span class="cart-order-price">$${(parseFloat(t.price.replace("$",""))*t.quantity).toFixed(2)}</span>
           <div class="cart-order-amount">
-            <button class="cart-order-decr" type="button">-</button>
+            <button class="cart-order-decr" type="button"><svg class="minus-icon" width="18" height="18">
+                <use
+                  class="cart-remove-svg"
+                  xlink:href="${s}#minus"
+                ></use>
+              </svg></button>
             <span class="cart-order-quantity">${t.quantity}</span>
-            <button class="cart-order-incr" type="button">+</button>
+            <button class="cart-order-incr" type="button"><svg class="plus-icon" width="18" height="18">
+                <use
+                  class="cart-remove-svg"
+                  xlink:href="${s}#plus"
+                ></use>
+              </svg></button>
           </div>
         </div>
       </div>
     </div>
   </li>
-  `).join("")}function i(){r.length?c.innerHTML=b(r):(q.classList.remove("visually-hidden"),x.classList.add("visually-hidden"),S.classList.add("visually-hidden"))}function _(e){const t=e.closest(".cart-order-item"),n=Array.from(c.children).indexOf(t);r[n].quantity>1&&(r[n].quantity-=1,g(t,r[n]))}function I(e){const t=e.closest(".cart-order-item"),n=Array.from(c.children).indexOf(t);r[n].quantity+=1,g(t,r[n])}function y(e){const t=e.target.closest(".cart-order-item");if(t){const n=Array.from(c.children).indexOf(t);r.splice(n,1)[0],l(),t.remove(),a(),i(),d()}}c.addEventListener("click",e=>{e.target.closest(".cart-remove-btn")&&y(e)});const E=document.querySelector(".header__js_span");function T(){r=[],c.innerHTML="",a(),l(),i(),d(),E.textContent=`CART (${0})`}function g(e,t){const n=e.querySelector(".cart-order-price"),C=e.querySelector(".cart-order-quantity"),h=parseFloat(t.price.replace("$",""));n.textContent=`$${(h*t.quantity).toFixed(2)}`,C.textContent=t.quantity,a(),l()}function a(){const e=r.reduce((t,n)=>t+parseFloat(n.price.replace("$",""))*n.quantity,0);Number(e.toFixed(2))}function l(){localStorage.setItem("cartItems",JSON.stringify(r))}document.addEventListener("DOMContentLoaded",()=>{i(),a(),d(),c.addEventListener("click",L),$.addEventListener("click",T)});function d(){r.length?(m.textContent=`Cart (${r.length})`,p.textContent=`Cart (${r.length})`):(p.textContent=`Cart (${0})`,m.textContent=`Cart (${0})`)}
+  `).join("")}function o(){r.length?a.innerHTML=b(r):($.classList.remove("visually-hidden"),x.classList.add("visually-hidden"),S.classList.add("visually-hidden"))}function I(e){const t=e.closest(".cart-order-item"),c=Array.from(a.children).indexOf(t);r[c].quantity>1&&(r[c].quantity-=1,h(t,r[c]))}function E(e){const t=e.closest(".cart-order-item"),c=Array.from(a.children).indexOf(t);r[c].quantity+=1,h(t,r[c])}function v(e){const t=e.target.closest(".cart-order-item");if(t){const c=Array.from(a.children).indexOf(t);r.splice(c,1)[0],i(),t.remove(),n(),o(),l()}}a.addEventListener("click",e=>{e.target.closest(".cart-remove-btn")&&v(e)});function O(){r=[],a.innerHTML="",n(),i(),o(),l()}function h(e,t){const c=e.querySelector(".cart-order-price"),y=e.querySelector(".cart-order-quantity"),f=parseFloat(t.price.replace("$",""));c.textContent=`$${(f*t.quantity).toFixed(2)}`,y.textContent=t.quantity,n(),i()}function n(){const e=r.reduce((t,c)=>t+parseFloat(c.price.replace("$",""))*c.quantity,0);Number(e.toFixed(2))}function i(){localStorage.setItem("cartItems",JSON.stringify(r))}document.addEventListener("DOMContentLoaded",()=>{o(),n(),l(),a.addEventListener("click",L),q.addEventListener("click",O)});function l(){r.length?(p.textContent=`Cart (${r.length})`,g.textContent=`Cart (${r.length})`):(g.textContent=`Cart (${0})`,p.textContent=`Cart (${0})`)}
 //# sourceMappingURL=commonHelpers.js.map
