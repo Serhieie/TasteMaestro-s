60aae9576ddf2910aebca767be43838c6ff50327
taniadavy <tania111davidenko111@gmail.com>
2023-12-10 12:12:11 +0100

    Modal-card-window without cart logic

---
+++ src/js/modal-product-card.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+
+const productListContainer = document.querySelector("#productsList");
+const modalCard = document.querySelector(".js-modal-product-card");
+const backdrop = document.querySelector(".js-backdrop");
+const loader = document.querySelector(".loader-container")
+
+productListContainer.addEventListener("click", onClickProductCard);
+
+modalCard.classList.add("visually-hidden");
+
+async function onClickProductCard(event){
+
+
+    if (event.target === event.currentTarget){
+        return;
+    };
+    const element = event.target.closest(".product__list__card");
+    if (element === null) {
+        return;
+    }
+    const elementId = element.id;
+
+    loader.classList.remove("visually-hidden")
+
+    const cardInfo = await getProductCardInfo(elementId);
+
+    const {category, desc, img, name, popularity, price, size} = cardInfo
+
+    const modalCardMarkup = createMarkupProductCard(category, desc, img, name, popularity, price, size);
+    modalCard.innerHTML = modalCardMarkup;
+    loader.classList.add("visually-hidden")
+    modalCard.classList.remove("visually-hidden");
+
+    const closeBtn = document.querySelector(".js-btn-close-card");
+    closeBtn.addEventListener("click", onClick);
+    backdrop.addEventListener("click", backdropOnClick);
+    document.addEventListener("keydown", onEscape);
+
+    function onClick() {
+        backdrop.classList.add("visually-hidden");
+        modalCard.classList.add("visually-hidden");
+        closeBtn.removeEventListener("click", onClick);
+    };
+    
+    function backdropOnClick() {
+        backdrop.classList.add("visually-hidden");
+        modalCard.classList.add("visually-hidden");
+        backdrop.removeEventListener("click", backdropOnClick);
+    };
+    
+    function onEscape(evt) {
+        evt.preventDefault();
+        if (evt.key === "Escape") {
+            backdrop.classList.add("visually-hidden");
+            modalCard.classList.add("visually-hidden");
+            document.removeEventListener("keydown", onEscape);
+        }
+    };    
+};
+
+async function getProductCardInfo(id) {
+    try{
+        modalCard.classList.remove("visually-hidden");
+        backdrop.classList.remove("visually-hidden")
+    
+        const BASE_URL = "https://food-boutique.b.goit.study/api";
+        const ENDPOINT = "products";
+        const {data} = await axios.get(`${BASE_URL}/${ENDPOINT}/${id}`);
+        return data;
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+function createMarkupProductCard(category, desc, img, name, popularity, price, size) {
+    const placeholderImg = "./img/image-placeholder.png"
+    return `<div class="product-container">
+    <div class="product-image-box">
+        <img class="product-image" src="${img !== null ? img : placeholderImg}" alt="${name}">
+    </div>
+    <div class="product-descr">
+        <h4 class="product-title">${name}</h4>
+        <ul class="product-details-list">
+            <li class="product-details-text"><span class="span-details-text">Category: </span>${category}</li>
+            <li class="product-details-text"><span class="span-details-text">Size: </span>${size}</li>
+            <li class="product-details-text"><span class="span-details-text">Popularity: </span>${popularity}</li>
+        </ul>
+        <p class="product-text">${desc}</p>
+    </div>
+    </div>
+    <div class="product-wraper">
+    <span class="product-price">${price}</span>
+    <button class="product-btn-shopping-cart" type="submit">
+        <span>Add to</span>
+        <svg class="product-cart-icon" width="18" height="18">
+            <use href="./img/sprite.svg#shopping-cart"></use>
+        </svg>
+    </button>
+    </div>
+    <button class="modal-btn-close js-btn-close-card" type="button">
+    <svg class="icon-close">
+        <use href="./img/sprite.svg#close-cross"></use>
+      </svg>
+    </button>`;
+};
--- src/js/modals.js
+++ src/js/modals.js
@@ -8,12 +8,19 @@ const refs = {
     closeBtn: document.querySelectorAll(".js-close"),
 }
 
-function onClick() {
+function onClickFirstCase() {
     refs.backdrop.classList.add("visually-hidden");
     refs.modalFirstCase.classList.add("visually-hidden");
+    BtnFistCase.removeEventListener("click", onClickFirstCase);
 };
 
-function onEscape(evt) {
+function onClickSecondCase() {
+    refs.backdrop.classList.add("visually-hidden");
+    refs.modalSecondCase.classList.add("visually-hidden");
+    BtnSecondCase.removeEventListener("click", onClickSecondCase);
+};
+
+function onEscapeFirstCase(evt) {
     evt.preventDefault();
     if (evt.key === "Escape") {
         refs.backdrop.classList.add("visually-hidden");
@@ -22,21 +29,35 @@ function onEscape(evt) {
     }
 };
 
-function backdropClick () {
-    refs.backdrop.classList.add("visually-hidden");
-    refs.modalFirstCase.classList.add("visually-hidden");
+function onEscapeSecondCase(evt) {
+    evt.preventDefault();
+    if (evt.key === "Escape") {
+        refs.backdrop.classList.add("visually-hidden");
+        refs.modalSecondCase.classList.add("visually-hidden");
+        document.removeEventListener("keydown", onEscape);
+    }
 };
 
-console.log(refs.closeBtn)
+function backdropFirstCase() {
+    refs.backdrop.classList.add("visually-hidden");
+    refs.modalFirstCase.classList.add("visually-hidden");
+    refs.backdrop.removeEventListener("click", backdropFirstCase);
+};
+
+function backdropSecondCase() {
+    refs.backdrop.classList.add("visually-hidden");
+    refs.modalSecondCase.add("visually-hidden");
+    refs.backdrop.removeEventListener("click", backdropSecondCase);
+};
 
 function showModalFirstCase() {
     refs.backdrop.classList.remove("visually-hidden");
     refs.modalFirstCase.classList.remove("visually-hidden");
 
     const BtnFistCase = refs.closeBtn[0];
-    BtnFistCase.addEventListener("click", onClick);
-    document.addEventListener("keydown", onEscape);
-    refs.backdrop.addEventListener("click", backdropClick);
+    BtnFistCase.addEventListener("click", onClickFirstCase);
+    document.addEventListener("keydown", onEscapeFirstCase);
+    refs.backdrop.addEventListener("click", backdropFirstCase);
 };
 
 function showModalSecondCase() {
@@ -44,14 +65,9 @@ function showModalSecondCase() {
     refs.modalSecondCase.classList.remove("visually-hidden");
 
     const BtnSecondCase = refs.closeBtn[1];
-    BtnSecondCase.addEventListener("click", onClick);
-    document.addEventListener("keydown", onEscape);
-    refs.backdrop.addEventListener("click", backdropClick);
+    BtnSecondCase.addEventListener("click", onClickSecondCase);
+    document.addEventListener("keydown", onEscapeSecondCase);
+    refs.backdrop.addEventListener("click", backdropSecondCase);
 }
 
-
-
 export {showModalFirstCase, showModalSecondCase}
-
-
-
